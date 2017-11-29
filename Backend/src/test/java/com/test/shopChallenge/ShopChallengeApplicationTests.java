20930e29968126e1cb0887addd1153a25ec112b1
zaani youssef <zaani.youssef@gmail.com>
2017-11-29 16:16:30 +0000

    Create new project spring boot
    - Create new spring starter project
    - Add Web + MongoDB + Embdded MongoDB

---
+++ Backend/src/test/java/com/test/shopChallenge/ShopChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.shopChallenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
